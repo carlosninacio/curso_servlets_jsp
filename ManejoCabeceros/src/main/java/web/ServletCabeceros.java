4543c413011e6e24c4c108b019ab3cab83d69e39
carlosninacio <cigramirez@poligran.edu.co>
2026-03-17 14:19:28 -0500

    index y servlet mostrando metodo http y uri

---
+++ ManejoCabeceros/src/main/java/web/ServletCabeceros.java
@@ -0,0 +1,54 @@
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "ServletCabeceros", urlPatterns = {"/ServletCabeceros"})
+public class ServletCabeceros extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String metodoHttp = request.getMethod();
+        
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Cabeceros Http</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Cabeceros Http</h1>");
+            out.println("<br>");
+            out.println("Método Http: " + metodoHttp);
+            out.println("<br>");
+            out.println("Uri solicitada: " + request.getRequestURI());
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
