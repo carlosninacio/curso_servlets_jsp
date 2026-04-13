3d72054400e9a939a52e3dfad73b7531cf780f41
carlosninacio <cigramirez@poligran.edu.co>
2026-04-13 15:39:53 -0500

    String con la sentencia SQL de SELECT

--- ControlClientes/src/main/java/datos/ClienteDAO.java
+++ ControlClientes/src/main/java/datos/ClienteDAO.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package datos;
-
-/**
- *
- * @author Nacho
- */
 public class ClienteDAO {
+    // CRUD - Create - Read - Update - Delete
+    
     
 }
